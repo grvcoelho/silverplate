9005541ed7ab7fcb1da0fde2b17da4847c1fbd78
Guilherme Rv Coelho <guilhermervcoelho@gmail.com>
2016-12-24 12:30:15 -0200

    Add new eslint config

--- webpackfile.js
+++ webpackfile.js
@@ -23,8 +23,14 @@ const rules = [
   },
   {
     test: /\.js$/,
-    use: ['babel-loader'],
-    exclude: /node_modules/
+    exclude: /node_modules/,
+    use: ['babel-loader']
+  },
+  {
+    test: /\.js$/,
+    enforce: 'pre',
+    exclude: /node_modules/,
+    use: ['eslint-loader']
   },
   {
     test: /\.css$/,
